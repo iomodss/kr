f6636f63000d3ba293285156ac76b6a5cccd97e4
iomodss <51040128+iomodss@users.noreply.github.com>
2019-05-29 05:00:38 +0300

    fix2

--- y6.js
+++ y6.js
@@ -351,4 +351,4 @@ Haxy.prototype.roundN = function (a, b) {
 };
 window.haxy = new Haxy;
 "1.2" !== document.querySelector("meta[name*=gpy_version]").content && (alert("[NEW] Download new HAXY script."), location.href = "https://iomods.org");
-0 > document.querySelector("script[src*='js/____']").src.indexOf("3LsJh") && (alert("[UPDATED] Download from. https://iomods.org."), location.href = "https://iomods.org");
+0 > document.querySelector("script[src*='js/____']").src.indexOf("xxfBl") && alert("[HACK IS ON UPDATING PROGRESS] CHECK DAILY or TRY DOWNLOAD FROM. https://iomods.org");
